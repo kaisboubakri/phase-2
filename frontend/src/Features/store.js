a642518f98a3b6a7fafbfd36bfc289f27c5b3471
kaisboubakri <kais.boubakri1998@gmail.com>
2024-03-03 19:33:18 +0100

    updated

--- frontend/src/Features/store.js
+++ frontend/src/Features/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSliceReducer from "./themeSlice";
-import refreshSidebar from "./refreshSidebar";
 
 export const store = configureStore({
   reducer: {
     themeKey: themeSliceReducer,
-    refreshKey: refreshSidebar,
   },
 });
